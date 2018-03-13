23bebd7c6c7c7aca612012540d9ca97df9f8d78b
Jose Platas <jplatas@outlook.com>
2018-03-12 23:58:59 -0700

    Inital commit to demostrate babel and react use

---
+++ js/component-survey.js
@@ -0,0 +1,17 @@
+
+class Test extends React.Component{
+
+  render(){
+    return(
+      <div>
+        <h1>Test Form</h1>
+      </div>
+    )
+  }
+}
+
+//Render the Dom
+ReactDOM.render(
+	<Test />,
+	document.getElementById("main")
+);
