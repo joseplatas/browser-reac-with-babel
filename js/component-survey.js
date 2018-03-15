cff1543563ee6f8b63fd49f527581f4e1723e764
Jose Platas <jplatas@outlook.com>
2018-03-15 00:16:56 -0700

    Added a second functional component and a switch statement for different type of questions

--- js/component-survey.js
+++ js/component-survey.js
@@ -10,11 +10,11 @@ function InformationSurvey(props) {
 
 function SelectQuestion(props){
   return(
-    <div id="q1" class="question_wrapper">
-      <div class="question required">
-        <span class="number">1</span>  Based on your recent repair, how would you rate your overall experience?
+    <div className="question_wrapper">
+      <div className="question required">
+        <span className="number">{props.id}</span> {props.data.question_text}
       </div>
-      <select class="not-custom">
+      <select>
         <option value="" disabled selected>Rate from 1 to 10</option>
         <option value="1">1</option>
         <option value="2">2</option>
@@ -30,20 +30,46 @@ function SelectQuestion(props){
     </div>
   );
 }
+
+function InputQuestion(props){
+  return(
+    <div className="question_wrapper">
+      <div className="question required">
+        <span className="number">{props.id}</span> {props.data.question_text}
+      </div>
+      <input type="text" />
+    </div>
+  );
+}
 //survey form builder
 class SurveyForm extends React.Component{
 
-  build_quiestions(){
-    console.log(this.props);
-    return (
-      <SelectQuestion />
-    );
+  build_questions(){
+    let qComponents = [];
+    //traverse questions
+    for (var i = 0; i < this.props.questions.length; i++) {
+      let q = this.props.questions[i];
+      let id = i + 1;
+      switch (q.type) {
+        case 'select':
+            qComponents.push(<SelectQuestion id={id} data={q}/>);
+          break;
+        case 'text':
+          qComponents.push(<InputQuestion id={id} data={q}/>);
+          break;
+        default:
+          //nothing
+      }
+
+    }
+
+    return qComponents;
   }
 
   render(){
     return(
       <form id="customer_survey_form" action="#" method="post">
-        {this.build_quiestions()}
+        {this.build_questions()}
       </form>
     )
   }
@@ -65,7 +91,19 @@ class Survey extends React.Component{
 const survey_data = {
   title: "Survey",
   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas suscipit ex at odio convallis, et feugiat est feugiat. Etiam lobortis dui vehicula sollicitudin volutpat. Mauris hendrerit finibus purus eget vehicula",
-  questions: [{},{}]
+  questions: [{
+    question_text: "Question for the select",
+    type: "select",
+    options: [1,2,3,4,5,6,7,8,9,10],
+    isRequired: true
+  },
+  {
+    question_text: "Question for text input",
+    type: "text",
+    options: [],
+    isRequired: false
+  }
+  ]
 }
 //Render the Dom
 ReactDOM.render(
